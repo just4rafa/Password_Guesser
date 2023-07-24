f72851e2f40a6c6c2d4d8a9a055242b83912dfb3
just4rafa <rafajust4@gmail.com>
2023-07-24 18:24:47 +0300

    I upgraded the code and now you can see how many tries it took to find your password

--- Password_Guesser.py
+++ Password_Guesser.py
@@ -8,12 +8,20 @@ chars_list = list(chars)
 
 password = pyautogui.password("Enter a password : ")
 guess_password = ""
+ 
+len_passwd = len(password)
+nr_ev_loops = 0
+nr_loops = 0
 
 while(guess_password != password):
+    nr_ev_loops += 1
     guess_password = random.choices(chars_list, k = len(password))
 
     print("<====================" + str(guess_password) + "====================>")
+    if nr_ev_loops == len_passwd:
+        nr_ev_loops = 0
+        nr_loops +=1
 
     if (guess_password == list(password)):
-        print("Your password is : " + "".join(guess_password))
+        print("Your password is : " + "".join(guess_password) + " | Guessed in " + str(nr_loops) + " tries")
         break
